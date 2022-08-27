c6795482a1352949a192f520e4694579127e70c8
Mohsin Khan <mohsinsmannat@gmail.com>
2022-08-27 14:38:54 +0530

    Added code to Display Data

--- app/src/main/java/com/itsmohsin/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/itsmohsin/retrofitdemo/MainActivity.kt
@@ -2,10 +2,31 @@ package com.itsmohsin.retrofitdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import okhttp3.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val retService = RetrofitInstance
+            .getRetrofitInstance()
+            .create(AlbumService::class.java)
+        val responseLiveData = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if(albumsList!=null){
+                while(albumsList.hasNext()){
+                    val albumsItem= albumsList.next()
+                    Log.i("MyTag", albumsItem.title)
+                }
+            }
+        })
     }
 }
