3e4a2602a25e58d99094b1504c65c41afb864e94
Mohsin Khan <mohsinsmannat@gmail.com>
2022-08-27 15:12:44 +0530

    Added Query Parameters to App

--- app/src/main/java/com/itsmohsin/retrofitdemo/AlbumService.kt
+++ app/src/main/java/com/itsmohsin/retrofitdemo/AlbumService.kt
@@ -2,9 +2,13 @@ package com.itsmohsin.retrofitdemo
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AlbumService {
     @GET("/albums")
 //    using kotlin coroutines so suspend is used here
     suspend fun getAlbums() : Response<Albums>
+
+    @GET("/albums")
+    suspend fun getSortedAlbums(@Query("userId") userId:Int) : Response<Albums>
 }
