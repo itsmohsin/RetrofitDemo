75dab0a9bfc51025696c464c72da0c398f523cd2
Mohsin Khan <mohsinsmannat@gmail.com>
2022-08-27 15:27:42 +0530

    Added Retrofit Path Parameters to App

--- app/src/main/java/com/itsmohsin/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/itsmohsin/retrofitdemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.itsmohsin.retrofitdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
@@ -18,6 +19,18 @@ class MainActivity : AppCompatActivity() {
         val retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
+
+        //path parameter example
+        val pathResponse = liveData{
+            val response = retService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext,title,Toast.LENGTH_LONG).show()
+        })
+
         val responseLiveData = liveData {
             val response = retService.getSortedAlbums(3)
             emit(response)
